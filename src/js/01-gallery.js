4440f5c9e0202acef0577046692cc80cad4a3962
Ivan Osadchuk <osadchukua@gmail.com>
2022-10-08 11:02:35 +0200

    final

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+console.log(galleryItems);
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) =>
+    acc +
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`,
+  ''
+);
+
+list.insertAdjacentHTML('afterbegin', markup);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+const LOCALSTORAGE_KEY_FORM = 'feedback-form-state';
+// const formData = {};
+const form = document.querySelector('.feedback-form');
+const message = document.querySelector('textarea');
+const email = document.querySelector('input');
 
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(evt) {
+  // formData[evt.target.name] = evt.target.value;
+  const formData = { email: email.value, message: message.value };
+  // console.log(formData);
+  localStorage.setItem(LOCALSTORAGE_KEY_FORM, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY_FORM)));
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY_FORM);
+}
+
+function cheksStorage() {
+  const parseSavedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY_FORM));
+  // console.log(localStorage.getItem(LOCALSTORAGE_KEY_FORM));
+  // console.log(parseSavedData);
+  if (parseSavedData) {
+    email.value = parseSavedData.email;
+    message.value = parseSavedData.message;
+  } else {
+    email.value = '';
+    message.value = '';
+  }
+}
+cheksStorage();
